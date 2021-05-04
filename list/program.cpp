50847e63501469b5bb83803b28142a5c36f0e560
immarchz <46131013+immarchz@users.noreply.github.com>
2021-05-04 18:49:32 +0700

    UpdateProgram.cpp

--- list/program.cpp
+++ list/program.cpp
@@ -10,7 +10,19 @@ void main()
 	mylist.pushToHead('e');
 	mylist.pushToHead('n');
 	mylist.print();
+	
+	//reverse words
+	mylist.reverse();
+	
 
+	cout << "\nEnter find alphabet: ";
+	cin >> f;
+	if (mylist.search(f)) {
+		cout << "Found";
+	}
+	else {
+		cout << "Not Found";
+	}
 	//TO DO! Write a program that tests your list library - the code should take characters, push them onto a list, 
 	//- then reverse the list to see if it is a palindrome!
 	
